ddbfc787aacfddb1fba198c8f52ca3ab49ebf035
Zola <Zelalemyohannes75@gmail.com>
2024-08-10 12:35:30 +0300

    cmmited

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 </script>
 
 <template>
-    <div class="grid gap-8 h-screen">
+    <div class="grid gap-8">
         <header class="flex items-start justify-between">
 
             <div class="grow">
@@ -17,7 +17,38 @@
         </header>
 
         <main class="grid gap-2">
-            <div class="flex items-center gap-3">
+
+            <template>
+                <Tabs default-value="account" class="w-[400px]">
+                    <TabsList>
+                        <TabsTrigger value="today">
+                            Today
+                        </TabsTrigger>
+                        <TabsTrigger value="week">
+                            This Week
+                        </TabsTrigger>
+                        <TabsTrigger value="month">
+                            This month
+                        </TabsTrigger>
+                        <TabsTrigger value="year">
+                            This year
+                        </TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="today">
+                        This is today
+                    </TabsContent>
+                    <TabsContent value="week">
+                        This Week
+                    </TabsContent>
+                   <TabsContent value="month">
+                        This Month
+                    </TabsContent>
+                    <TabsContent value="year">
+                        This year
+                    </TabsContent>
+                </Tabs>
+            </template>
+            <!-- <div class="flex items-center gap-3">
                <div v-for='(item, index) in 3' :key="index" class="w-[120px] h-[36px] bg-neutral-200">
 
                </div>
@@ -25,16 +56,16 @@
 
             <section>
                 <div class="w-full h-[360px] bg-neutral-200"></div>
-            </section>
+            </section> -->
         </main>
 
         <footer>
-         <div class="flex items-center gap-3">
-            <div v-for="(item, index) in 3" :key="index" class="w-full h-[260px] bg-neutral-200">
+            <div class="flex items-center gap-3">
+                <div v-for="(item, index) in 3" :key="index" class="w-full h-[260px] bg-neutral-200">
+
+                </div>
 
             </div>
-
-         </div>
         </footer>
 
 
